0df8e78957c45273714b3e225160eeb911804e31
Daniel <danieltheron11@proton.me>
2025-08-19 15:29:14 +0200

    Update main.rs
    added the random generator and also the string to number converter and also the thing that compares the two varibales

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@
 
 use std::io; //imports standard user io library (why does this need to be done its so simple in python)
 
+use std::cmp::Ordering; // added library std, the standard library. comp, probably stnads for computing. Ordering is for comparing numbers
+
 use rand::Rng; // imports random library so u can get random. Rng is apparently how it generates the random number
 
 fn main() { // main function
     println!("Guess the number!"); // basically print on python but its got the ! which means its a rust "macro" not a normal function
     
-    let secret_number = rand::thread_rng().genrange(1..=100); //i see no mut meaning the variable is not going to change during the program. calls on the random library and uses rng to generate no from 1 - 100 using .genrange(1..=100) method
+    let secret_number = rand::thread_rng().gen_range(1..=100); //i see no mut meaning the variable is not going to change during the program. calls on the random library and uses rng to generate no from 1 - 100 using .genrange(1..=100) method
 
     println!("PLease Input your guess");
 
@@ -21,6 +23,13 @@ fn main() { // main function
         .expect("failed to read line"); // error handling. notice no ";" after the readline, this is a single code thingy, just u can press enter to make it look better
         // this whole code thingy could look like io::stdin().read_line(&mut guess).expect("failed to read line");
 
-    println!("you guessed: {guess}") // just prints out the user input. {} are like crab pincers to hold something in place
+    let guess: u32 = guess.trim().parse().expect("You didnt type a number, please type one!");
+
+    println!("you guessed: {guess}"); // just prints out the user input. {} are like crab pincers to hold something in place
     
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
