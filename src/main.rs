a93865ae55f3f2ac11fe06f594de8849c3a7a7bb
DJ_Theron <97227143+DJTheron@users.noreply.github.com>
2025-08-28 18:01:03 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 use std::io; //imports standard user io library (why does this need to be done its so simple in python)
 
-se std::cmp::Ordering; // added library std, the standard library. comp, probably stnads for computing. Ordering is for comparing numbers
+use std::cmp::Ordering; // added library std, the standard library. comp, probably stnads for computing. Ordering is for comparing numbers
 
 use rand::Rng; // imports random library so u can get random. Rng is apparently how it generates the random number
 
